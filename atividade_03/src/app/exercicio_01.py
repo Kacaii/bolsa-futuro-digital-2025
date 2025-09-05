93c580c66887959b9e42fe9cb0a3ae59e3b5b9cc
Kacaii <kacaii.privado@gmail.com>
2025-09-05 18:05:18 -0300

    finish exercicio 1

--- atividade_03/src/app/exercicio_01.py
+++ atividade_03/src/app/exercicio_01.py
@@ -1,6 +1,14 @@
 def main():
-    num1 = input("Insira um número:\n > ")
-    num2 = input("Insira outro número:\n > ")
+    # input --------------------------------------------------------------------
+    primeiro_numero: float = float(input("Insira um número:\n > "))
+    segundo_numero: float = float(input("Insira outro número:\n > "))
+
+    resultado_soma = primeiro_numero + segundo_numero
+    resultado_subtracao = primeiro_numero - segundo_numero
+
+    # output -------------------------------------------------------------------
+    print(f"num1 + num2 = {resultado_soma}")
+    print(f"num1 + num2 = {resultado_subtracao}")
 
 
 if __name__ == "__main__":
